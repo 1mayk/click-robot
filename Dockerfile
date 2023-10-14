c34e5f147055f1c05281f276ec115e05b846ffe4
Mayk <maycon.lrgo@gmail.com>
2023-10-14 16:21:40 -0300

    feat: tentando sem navegadores

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,5 @@
-# Use a imagem base do Python 3.12
+# Use a imagem base do Python
 FROM python:3.12
-
-# Instale as dependências necessárias
-RUN apt-get update && apt-get -y install wget curl unzip gnupg
-
-# Adicione o repositório do Google Chrome e sua chave
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-RUN echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
-
-# Instale o Google Chrome
-RUN apt-get update && apt-get -y install google-chrome-stable
-
-# Baixe e instale o ChromeDriver manualmente
-RUN CHROMEDRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-    wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip -P / && \
-    unzip /chromedriver_linux64.zip -d / && \
-    chmod +x /chromedriver && \
-    mv -f /chromedriver /usr/local/share/ && \
-    ln-s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 
 # Defina o diretório de trabalho
 WORKDIR /app
@@ -28,5 +10,5 @@ COPY . .
 # Instale as dependências Python
 RUN pip install -r requirements.txt
 
-# Execute o script "robot"
+# Defina o comando para executar o script
 CMD ["python", "robot.py"]
--- robot.py
+++ robot.py
@@ -3,8 +3,11 @@ from bs4 import BeautifulSoup
 import time
 
 url = "https://clickhelp.org/me/cliente"
-
 session = requests.Session()
+
+# Dados de login
+email = "maycon.lrgo@gmail.com"
+password = "12345678"
 
 while True:
     # Fazer uma solicitação GET para a página
@@ -14,33 +17,30 @@ while True:
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
 
-        # Encontrar o campo de email pelo ID
+        # Encontrar os campos de email e senha
         email_input = soup.find("input", {"id": "email"})
-        if email_input:
-            email_input_value = email_input["value"]
-            email_input_value = "maycon.lrgo@gmail.com"
-
-        # Encontrar o campo de senha pelo ID
         password_input = soup.find("input", {"id": "password"})
-        if password_input:
-            password_input_value = password_input["value"]
-            password_input_value = "12345678"
 
-        # Encontrar o botão de login pela classe
-        login_button = soup.find("button", {"class": "enter__login"})
-        if login_button:
-            # Fazer uma solicitação POST para o formulário de login
-            form_data = {
-                "email": email_input_value,
-                "password": password_input_value,
-            }
-            login_url = "https://clickhelp.org/me/login"  # URL de login
-            response = session.post(login_url, data=form_data)
+        if email_input and password_input:
+            # Extrair os valores dos campos (se necessário)
+            email_input["value"] = email
+            password_input["value"] = password
 
-            if response.status_code == 200:
-                print("Login bem-sucedido.")
-            else:
-                print("Falha no login.")
+            # Encontrar o botão de login pela classe
+            login_button = soup.find("button", {"class": "enter__login"})
+
+            if login_button:
+                # Fazer uma solicitação POST para o próprio URL da página
+                form_data = {
+                    "email": email,
+                    "password": password,
+                }
+                response = session.post(url, data=form_data)
+
+                if response.status_code == 200:
+                    print("Login bem-sucedido.")
+                else:
+                    print("Falha no login.")
 
     print("click")
 
