fc3e9523c5eb98a57c6daa59e4e23a5798614c57
Mayk <maycon.lrgo@gmail.com>
2023-10-14 17:22:38 -0300

    feat: voltando para o selenium

--- robot.py
+++ robot.py
@@ -1,48 +1,25 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 url = "https://clickhelp.org/me/cliente"
-session = requests.Session()
 
-# Dados de login
-email = "maycon.lrgo@gmail.com"
-password = "12345678"
+driver = webdriver.Chrome()
 
 while True:
-    # Fazer uma solicitação GET para a página
-    response = session.get(url)
+    driver.get(url)
 
-    # Verificar se a solicitação foi bem-sucedida
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
+    email_input = driver.find_element(By.ID, "email")
+    email_input.send_keys("maycon.lrgo@gmail.com")
 
-        # Encontrar os campos de email e senha
-        email_input = soup.find("input", {"id": "email"})
-        password_input = soup.find("input", {"id": "password"})
+    password_input = driver.find_element(By.ID, "password")
+    password_input.send_keys("12345678")
 
-        if email_input and password_input:
-            # Extrair os valores dos campos (se necessário)
-            email_input["value"] = email
-            password_input["value"] = password
-
-            # Encontrar o botão de login pela classe
-            login_button = soup.find("button", {"class": "enter__login"})
-
-            if login_button:
-                # Fazer uma solicitação POST para o próprio URL da página
-                form_data = {
-                    "email": email,
-                    "password": password,
-                }
-                response = session.post(url, data=form_data)
-
-                if response.status_code == 200:
-                    print("Login bem-sucedido.")
-                else:
-                    print("Falha no login.")
+    login_button = driver.find_element(By.CLASS_NAME, "enter__login")
+    login_button.click()
 
     print("click")
 
-    # Esperar 300 segundos (5 minutos)
     time.sleep(300)
+
+driver.quit()
