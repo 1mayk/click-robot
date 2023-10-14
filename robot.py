cd1e0625453f007e098b9843621217528dfd6d43
Mayk <maycon.lrgo@gmail.com>
2023-10-14 02:38:25 -0300

    feat: robo ping completo

---
+++ robot.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+url = "https://clickhelp.org/me/cliente"
+
+driver = webdriver.Chrome()
+
+while True:
+    driver.get(url)
+
+    email_input = driver.find_element(By.ID, "email")
+    email_input.send_keys("maycon.lrgo@gmail.com")
+
+    password_input = driver.find_element(By.ID, "password")
+    password_input.send_keys("12345678")
+
+    login_button = driver.find_element(By.CLASS_NAME, "enter__login")
+    login_button.click()
+
+    print("click")
+
+    time.sleep(300)
+
+driver.quit()
